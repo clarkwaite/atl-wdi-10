c66b92f81dea66390ef25a630a8dc50ff916df0e
clarkwaite <clarkwaite@gmail.com>
2017-05-16 17:45:01 -0400

    "completed GET routes"

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -11,3 +11,18 @@ app.listen(port, function(){
   console.log('LISTENING ON PORT ' + port);
   console.log("==========================")
 });
+
+app.get('/', function(req, res) { 
+  // when a request comes in at localhost:3000, it will respond 
+  res.send('Welcome to Pizza Express!');
+});
+
+app.get('/topping/:type', function(req, res, next) {
+  // when a request comes in at localhost:3000, it will respond 
+  res.send(`${req.params.type} pizza! Good choice!`);
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+  // when a request comes in at localhost:3000, it will respond 
+  res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+});
