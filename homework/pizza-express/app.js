127d055695b6d18dd3e1631162dca9b4348e4f16
clarkwaite <clarkwaite@gmail.com>
2017-05-16 17:58:38 -0400

    set up hbs views

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -1,11 +1,11 @@
-//require express package
 var express = require('express');
-//save an express module as 'app'
 var app     = express();
-// assigning 3000 as our port
-var port    = 3000;
 
-// tells the server to listen for requests on port 3000
+var hbs = require('hbs');
+app.set("view engine", "hbs");
+app.set('views', './views');
+
+var port    = 3000;
 app.listen(port, function(){
   console.log("==========================")
   console.log('LISTENING ON PORT ' + port);
