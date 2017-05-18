62f0559409ee49d5515fcfa20cf0c89ef8447c84
clarkwaite <clarkwaite@gmail.com>
2017-05-18 14:45:07 -0400

    starting toDont work

--- morning_exercises/unit_02/pseudocode/script.js
+++ morning_exercises/unit_02/pseudocode/script.js
@@ -3,28 +3,36 @@
 // Barrels O' RUM
 
 var barrels = function(small, large, total){
-  var smallBarrel = 60;
-  var largeBarrel = (total - (small * smallBarrel)) / large;
-
-return largeBarrel;
+  var smallBarrelPrice = 60;
+  var largeBarrelTotalCost = (total - (small * smallBarrelPrice)) / large;
+  var largeBarrelPrice = largeBarrelTotalCost/large
+return largeBarrelPrice;
 };
-
+barrels(3,5,885)
 //*************************
 // Problem 2:
 // Sailing the Seas
 
 var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+  earthCircumference = 1000*24;
+  var dollarsPerMile = fuelPrice/milesPerGallon;
+  var costToTravelAroundTheWorld = dollarsPerMile*earthCircumference;
+  return costToTravelAroundTheWorld
 };
-
+shipFuelCost(3,12)
 //*************************
 // Problem 3:
 // GROG
 
 var calcFruitJuice = function(a, b, c, d){
-  //your code here
+  var totalJuice = a+c;
+  var costcoPunchPureJuiceGallons = b*a;
+  var KirklandPureJuiceGallons = d*c;
+  var totalGallonsOfPureFruitJuice = costcoPunchPureJuiceGallons+KirklandPureJuiceGallons;
+  var percentPureFruitJuice = (totalGallonsOfPureFruitJuice/totalJuice)/100
+  return percentPureFruitJuice;
 };
-
+calcFruitJuice(3,20,2,55)
 //DO NOT EDIT BELOW THIS LINE//
 module.exports = {
   barrels: barrels,
