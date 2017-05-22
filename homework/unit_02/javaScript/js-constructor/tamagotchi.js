babf801a3e10b294f0293cf4c7026cc7fd6f98e5
clarkwaite <clarkwaite@gmail.com>
2017-05-22 17:52:44 -0400

    Stage 2 complete

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,21 @@
 console.log('tamagotchi file is loaded');
 
 //your constructor function here
-
-
+var tamagotchi = function (name, creatureType) {
+  this.name = name;
+  this.creatureType= creatureType;
+  this.foodInTummy = 10;
+  this.restedness = 10;
+  this.health = 10;
+  this.cry = function(){
+      console.log("WAHH!!! "+this.name+" is crying!!")
+      this.foodInTummy--;
+      console.log("Tamagotchi's food level is now "+this.foodInTummy+"/10!");
+  };
+};
 //create new Tamagotchis
-
-
+var tom1 = new Tamagotchi('Ziggy', 'dog');
+var tom2 = new Tamagotchi('Grover', 'dog');
 //test out your Tamagotchies below via console.logs
+console.log(tom1);
+console.log(tom2);
