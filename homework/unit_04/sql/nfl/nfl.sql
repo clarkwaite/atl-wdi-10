dc06e655e21090c9b07a14debe10f2d82b935641
clarkwaite <clarkwaite@gmail.com>
2017-06-14 18:18:41 -0400

    "Commit: NFL - wow there's a lot of nfl data"

--- homework/unit_04/sql/nfl/nfl.sql
+++ homework/unit_04/sql/nfl/nfl.sql
@@ -25,9 +25,20 @@ not cover in class.
 <hr>
 
 5.  The team names and head coaches of the NFC North and AFC East
+
+    SELECT head_coach, name  FROM teams WHERE conference = 'AFC' AND division = 'East' OR conference = 'NFC' AND division = 'North';
+
 6.  The 50 players with the highest salaries
+
+    SELECT * FROM players ORDER BY salary DESC limit 50
+
 7.  The average salary of all NFL players
+
+    SELECT AVG(salary) FROM players;
+
 8.  The names and positions of players with a salary above 10_000_000
+
+    SELECT name, position FROM players WHERE salary > 10000000;
 
 <hr>
 &#x1F534; COMMIT 3<br>
@@ -57,3 +68,5 @@ id |         name         |            stadium            | division | conferenc
     2 | Drayton Florence            | CB       |  4000000 |       1
     3 | Shawne Merriman             | LB       |  4000000 |       1
     4 | Dwan Edwards                | DT       |  3800000 |       1
+
+    ORDER BY hit_pages DESC LIMIT 5
