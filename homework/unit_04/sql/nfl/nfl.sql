247174904e5a3a093df91f88339062a2bc36a459
clarkwaite <clarkwaite@gmail.com>
2017-06-14 19:59:18 -0400

    nfl complete

--- homework/unit_04/sql/nfl/nfl.sql
+++ homework/unit_04/sql/nfl/nfl.sql
@@ -1,7 +1,3 @@
-Some queries may require more than one command (i.e. you may need to get information about a team before you can complete a query for players). Test each command in PSQL to make sure it is correct.
-
-_Hint_: In order to do some parts you will need to research commands we did
-not cover in class.
 
 1.  List the names of all NFL teams
     
@@ -19,10 +15,6 @@ not cover in class.
 
     SELECT COUNT(id) FROM players;
 
-<hr>
-&#x1F534; COMMIT 2<br>
-"Commit: NFL - queried some NFL stuff"
-<hr>
 
 5.  The team names and head coaches of the NFC North and AFC East
 
@@ -40,33 +32,26 @@ not cover in class.
 
     SELECT name, position FROM players WHERE salary > 10000000;
 
-<hr>
-&#x1F534; COMMIT 3<br>
-"Commit: NFL - wow there's a lot of nfl data"
-<hr>
-
 9.  The player with the highest salary in the NFL
+
+    SELECT * FROM players ORDER BY salary DESC limit 1;
+
 10. The name and position of the first 100 players with the lowest salaries
+
+    SELECT name, position  FROM players ORDER BY salary ASC limit 100;
+
 11. The average salary for a DE in the nfl
+
+    SELECT AVG(salary) FROM players WHERE position = 'DE';
+
 12. The names of all the players on the Buffalo Bills
+
+    SELECT players.name, teams.name FROM players, teams WHERE players.team_id=teams.id AND teams.name = 'Buffalo Bills';
+
 13. The total salary of all players on the New York Giants
+
+    SELECT SUM(players.salary) FROM players, teams WHERE players.team_id=teams.id AND  teams.name = 'New York Giants';
+
 14. The player with the lowest salary on the Green Bay Packers
 
-_Hint_: In order to do some parts you will need to research commands we did
-not cover in class.
-
-id |         name         |            stadium            | division | conference |   head_coach   | active
-----+----------------------+-------------------------------+----------+------------+----------------+--------
-  1 | Buffalo Bills        | Ralph Wilson Stadium          | East     | AFC        | Doug Marrone   | t
-  2 | Miami Dolphins       | Sun Life Stadium              | East     | AFC        | Joe Philbin    | t
-  3 | New England Patriots | Gillette Stadium              | East     | AFC        | Bill Belichick | t
-  4 | New York Jets        | MetLife Stadium               | East     | AFC        | Rex Ryan       | t
-
-   id  |            name             | position |  salary  | team_id
-------+-----------------------------+----------+----------+---------
-    1 | Mario Williams              | DE       |  5900000 |       1
-    2 | Drayton Florence            | CB       |  4000000 |       1
-    3 | Shawne Merriman             | LB       |  4000000 |       1
-    4 | Dwan Edwards                | DT       |  3800000 |       1
-
-    ORDER BY hit_pages DESC LIMIT 5
+SELECT players.name FROM players, teams WHERE players.team_id=teams.id AND teams.name = 'Green Bay Packers' ORDER BY salary ASC LIMIT 1;
