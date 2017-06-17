499097fb6eb808a0a94fac9250565de42c96a2cc
clarkwaite <clarkwaite@gmail.com>
2017-06-17 12:28:30 -0400

    yay booleans!

---
+++ homework/unit_04/ruby/ruby-ninja/ninja_skills.rb
@@ -0,0 +1,22 @@
+Challenge 1: Booleans
+
+Create a variable a and set it equal to true
+Create a variable b and set it equal to false
+Write a statement using a and b that evaluates to false
+puts the result to the terminal
+Write a statement using a and b that evaluates to true
+puts the result to the terminal
+🎯 Commit -m "1. Boolean Skills"
+
+def even_or_odd(num)
+
+a = true
+b = false
+
+if num.even? == a
+    puts a
+elsif num.even? == b 
+    puts b
+end
+
+even_or_odd(50)
