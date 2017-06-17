c2745b0b3b6fff0bed2b3ba1435f50ca4ff04152
clarkwaite <clarkwaite@gmail.com>
2017-06-17 12:38:55 -0400

    nil!!!

--- homework/unit_04/ruby/ruby-ninja/ninja_skills.rb
+++ homework/unit_04/ruby/ruby-ninja/ninja_skills.rb
@@ -1,12 +1,12 @@
-Challenge 1: Booleans
+# Challenge 1: Booleans
 
-Create a variable a and set it equal to true
-Create a variable b and set it equal to false
-Write a statement using a and b that evaluates to false
-puts the result to the terminal
-Write a statement using a and b that evaluates to true
-puts the result to the terminal
-🎯 Commit -m "1. Boolean Skills"
+# Create a variable a and set it equal to true
+# Create a variable b and set it equal to false
+# Write a statement using a and b that evaluates to false
+# puts the result to the terminal
+# Write a statement using a and b that evaluates to true
+# puts the result to the terminal
+# 🎯 Commit -m "1. Boolean Skills"
 
 def even_or_odd(num)
 
@@ -20,3 +20,31 @@ elsif num.even? == b
 end
 
 even_or_odd(50)
+
+# Challenge 2: Nil
+
+# Create a variable of your choosing and set it equal to Nil
+# p the value of the variable to the terminal
+# puts the value of the variable to the terminal concatenate the string "nil was here".
+# print the value of the variable to the terminal
+# puts the class of the value of the variable to the terminal. Use the .class method. Look up this method on Google.
+# 🎯 Commit -m "2. Nil skills."
+
+notta = nil
+puts notta
+x = "#{notta} was here."
+puts x
+puts nil.class
+# am I missing something? This didn't work very well.
+
+# Challenge 3: Integers
+
+# Create a variable d and set it equal to a Fixnum
+# puts the value of d to the terminal
+# Create a variable e and set it equal to a Float
+# puts the value of e to the terminal
+# Set d equal to e on another line
+# puts the value of d to the terminal
+# 🎯 Commit -m "3. Integers Skills"
+
+
